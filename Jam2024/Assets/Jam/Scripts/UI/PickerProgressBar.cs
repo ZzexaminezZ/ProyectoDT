16c26986bdc50d8cd3749dd94cc5ed5c506146b4
Felipe Rubio <juan.feliperubiog@gmail.com>
2024-01-27 21:46:46 -0500

    Fix progress bar

--- Jam2024/Assets/Jam/Scripts/UI/PickerProgressBar.cs
+++ Jam2024/Assets/Jam/Scripts/UI/PickerProgressBar.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,11 +6,6 @@ public class PickerProgressBar : MonoBehaviour
     [SerializeField] private Color _defaultColor;
 
     [SerializeField] private Image _progressBar;
-
-    private void Start()
-    {
-        gameObject.SetActive(false);
-    }
 
     public void UpdateProgressBar(float progress)
     {
